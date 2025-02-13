957bc3fbecef5faedfaac3367843cc55b64a2f02
joshuawjchan <joshuawjchan@hotmail.com>
2025-02-13 10:59:04 +1000

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/top-secret-project-lol/",
+  base: "/marketplace-walker-30lbs/",
   plugins: [react()],
 });
